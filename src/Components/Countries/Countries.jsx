e10f0b39558493362e9e9e68452a3d29965e00a1
mariyamnavila <bibimariyamnavila@gmail.com>
2025-08-04 15:28:48 +0600

    added dynamic  country flags container

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -4,6 +4,8 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([])
+    const [visitedFlags, setVisitedFlags] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags,region,languages,currencies,cca3,flags,population,area')
@@ -11,15 +13,49 @@ const Countries = () => {
             .then(data => setCountries(data))
     })
 
+
+    const handleVisitedCountry = (country) => {
+
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries)
+    }
+
+
+    const handleVisitedFlags = flag => {
+        const newVisitedFlags = [...visitedFlags,flag];
+        setVisitedFlags(newVisitedFlags)
+    }
+
+    //remove item from an array in a state
+    //use filter to select all he elements except the one you want to remove
+
+
+
     return (
         <div>
             <h3>Countries {countries.length}</h3>
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="flag-container">
+                {
+                    visitedFlags.map((flag,idx)=><img key={idx} style={{width: '150px', height:'90px', borderRadius: '10px'}} src={flag}></img>)
+                }
+            </div>
+            {/* display countries */}
             <div className="country-container">
                 {
-                countries.map(country => <Country
-                    key={country.cca3}
-                    country={country}></Country>)
-            }
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountry={handleVisitedCountry}
+                        handleVisitedFlags={handleVisitedFlags}
+                        country={country}></Country>)
+                }
             </div>
         </div>
     );
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
     const { name, flags, population, area, cca3 } = country;
 
     const [visited, setVisited] = useState(false)
@@ -9,13 +9,17 @@ const Country = ({ country }) => {
         setVisited(!visited)
     }
 
+    const passWithParams = () => handleVisitedCountry(country)
+
     return (
-        <div className={`country ${visited ? 'visited' :'non-visited'}`}>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
             <img style={{ width: "100%", height: "160px", borderRadius: '10px' }} src={flags.png} alt="" />
-            <h2 style={{color: visited ? 'purple' : 'white'}} className='text'>Name: {name?.common}</h2>
+            <h2 style={{ color: visited ? 'purple' : 'white' }} className='text'>Name: {name?.common}</h2>
             <p className='text'>Population: {population}</p>
             <p className='text'>Area: {area}</p>
             <p className='text'><small>Code: {cca3}</small></p>
+            <button style={{ width: '100%', marginBottom: '8px' }} onClick={passWithParams}>Mark visited </button>
+            <button style={{ width: '100%', marginBottom: '8px' }} onClick={()=>handleVisitedFlags(country.flags.png)}>Add Flags  </button>
             <button style={{ width: '100%' }} onClick={handleVisited}>{visited ? "Visited" : 'Going'}</button>
             <div className='text'>
                 {visited ? 'I have visited This country' : 'I want to visit this country'}
